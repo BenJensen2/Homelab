01ced5b430022e728bda030164342202fdfb5a1d
BenJensen2 <btjensen@mtu.edu>
2021-12-25 06:04:47 -0600

    Production changed to Portfolio and added Bugs folder

---
+++ Portfolio/client/src/App.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './App.css';
+import HomePage from './views/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Portfolio/client/src/views/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomePage = (props) => {
+  return(
+    <div className="home-page">
+      Home Page
+    </div>
+  )
+}
+
+export default HomePage;
