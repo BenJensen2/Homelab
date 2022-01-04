97371c6a31776cef0a9a0ca64c16ec881df784a2
BenJensen2 <btjensen@mtu.edu>
2022-01-04 08:22:20 -0600

    Initial list

--- Projects/Software/Internal_Solutions/Planning/client/src/views/Weekly.jsx
+++ Projects/Software/Internal_Solutions/Planning/client/src/views/Weekly.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 const Weekly = () => {
   return (
     <div className='weekly'>
+      <ul>
+        <li><h2>Expected Expenses</h2></li>
+        <li><h3>Under $300</h3></li>
+        <li><h3>Over $300</h3></li>
+        <li><h2>Appointments</h2></li>
+        <li><h2>Gifts & Tithes</h2></li>
+        <li><h2>Total</h2></li>
+        <li><h2>Total</h2></li>
+      </ul>
       </div>
   )
 }
