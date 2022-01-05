c661b1865caede70893cc03b6000340464423afa
BenJensen2 <btjensen@mtu.edu>
2022-01-04 18:01:02 -0600

    Map dummy data & new item form

--- Projects/Software/Internal_Solutions/Planning/client/src/views/MainContent.jsx
+++ Projects/Software/Internal_Solutions/Planning/client/src/views/MainContent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import NewItem from '../components/NewItem'
 import Weekly from './Weekly'
 
 const MainContent = () => {
   return (
     <div className='main-content'>
+      <NewItem/>
       <Routes>
         <Route path="/weekly" element={<Weekly />}/>
       </Routes>
